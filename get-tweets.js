6804fde567435eac0530840b52005e2acb24c974
Brian Feister <brian.feister@scala.com>
2016-11-06 03:37:35 -0500

    fix counting bug

--- get-tweets.js
+++ get-tweets.js
@@ -15,14 +15,17 @@ var tweetTotal = 0;
 
 var q = '#critters';
 
+function tallyTweetsAndRetweets (tweets) {
+    tweetTotal += tweets.statuses.length;
+    tweets.statuses.forEach( (tweet) => {
+        tweetTotal += tweet.retweet_count;
+    });
+}
 
 function getTweetChunk (max_id) {
     return new Promise( (resolve, reject) => {
         client.get('search/tweets', {max_id: max_id, count: 99, q: q}, function(error, tweets, response) {
-            tweetTotal += tweets.statuses.length;
-            tweets.statuses.forEach( (tweet) => {
-                tweetTotal += tweet.retweet_count;
-            });
+            tallyTweetsAndRetweets(tweets);
             resolve(tweets);
         });
     })
@@ -30,16 +33,13 @@ function getTweetChunk (max_id) {
         if ( tweets.statuses.length > 98) {
             return getTweetChunk(tweets.statuses[tweets.statuses.length - 1].id);
         } else {
-            tweetTotal += tweets.statuses.length;
+            tallyTweetsAndRetweets(tweets);
             console.log('tweetTotal:',tweetTotal);
         }
     });
 }
 
 client.get('search/tweets', {count: 99, q: q}, function(error, tweets, response) {
-    tweetTotal += tweets.statuses.length;
-    tweets.statuses.forEach( (tweet) => {
-        tweetTotal += tweet.retweet_count;
-    });
+    tallyTweetsAndRetweets(tweets);
     getTweetChunk(tweets.statuses[tweets.statuses.length - 1].id)
 });
